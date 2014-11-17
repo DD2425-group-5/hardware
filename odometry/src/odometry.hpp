e80ac9c24835952e794bcf02b495dcff4c002c86
Michal Staniaszek <m.staniaszek@gmail.com>
2014-11-17 13:49:25 +0100

    Added publisher for odometry messages

--- odometry/src/odometry.hpp
+++ odometry/src/odometry.hpp
@@ -3,21 +3,22 @@
 #include "ras_arduino_msgs/ADConverter.h"
 #include "ras_arduino_msgs/Encoders.h"
 #include "std_msgs/Bool.h"
-#include "ir_sensors/IRDists.h"
+#include "hardware_msgs/IRDists.h"
+#include "hardware_msgs/Odometry.h"
 
 
 class Odometry {
 public:
-	Odometry(int argc, char *argv[]);
+    Odometry(int argc, char *argv[]);
 	
 private:
-	ros::Subscriber sub_sensor;	//sub to get distance values
-	ros::Publisher pub_motor;	//for the motor
-	ros::Subscriber sub_encoder;// for encoder feedback
-	ros::Subscriber sub_isTurning;// for encoder feedback
+    ros::Subscriber sub_sensor;	//sub to get distance values
+    ros::Subscriber sub_encoder;// for encoder feedback
+    ros::Subscriber sub_isTurning;// for encoder feedback
+    ros::Publisher pub_odom;
 	
-	void runNode();
-	void sensorCallback(const ir_sensors::IRDists msg);
-	void encoderCallback(const ras_arduino_msgs::Encoders feedback);
-	void isTurningCallback(const std_msgs::Bool msg);
+    void runNode();
+    void sensorCallback(const hardware_msgs::IRDists msg);
+    void encoderCallback(const ras_arduino_msgs::Encoders feedback);
+    void isTurningCallback(const std_msgs::Bool msg);
 };
